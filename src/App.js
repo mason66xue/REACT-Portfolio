1327a16275e7b02d8a7b37e865fb63121d053061
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-07 11:33:28 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './component/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
