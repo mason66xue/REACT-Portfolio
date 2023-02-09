b6c0cfb57277b1d660d32c5edee6e8565638f44b
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-02-08 22:36:38 -0500

    adding some styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Nav from './component/Nav/nav';
 import Footer from './component/Footer/index';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Footer/>
+      <Header/>
       
     </div>
   );
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,37 +1,32 @@
-// import React from "react";
-// import Nav from "../Nav";
+import React from "react";
+import Nav from "../Nav";
 
-// function Header(props) {
-//   const { currentTab, handleTabChange } = props;
+function Header(props) {
 
-//   return (
-//     <div>
-//       <section>
-//         <header className="flex-row px-1">
-//           <div>
-//             <h1>Ruochen Liu's Portfolio</h1>
-//           </div>
-//           <div>
-//             <Nav
-//               currentTab={currentTab}
-//               handleTabChange={handleTabChange}
-//             ></Nav>
-//           </div>
-//         </header>
-//       </section>
-//       <section class="hero">
-//         <div class="hero-cta">
-//           {/* <img
-//             src={coverImage}
-//             className="my-2"
-//             style={{ width: "100%" }}
-//             alt="cover"
-//           /> */}
 
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <section>
+        <header className="flex-row px-1">
+          <div>
+            <h1>Ruochen Liu's Portfolio</h1>
+          </div>
+          
+        </header>
+      </section>
+      <section class="hero">
+        <div class="hero-cta">
+          {/* <img
+            src={coverImage}
+            className="my-2"
+            style={{ width: "100%" }}
+            alt="cover"
+          /> */}
 
-// export default Header;
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
